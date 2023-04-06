e828ab3aed333a31fc440b5d211b0eb62c6df99f
mohsin6862 <mohsinctg2206@gmail.com>
2023-04-06 20:22:27 +0600

    Remove from cart included

--- src/components/OrderReview/OrderReview.jsx
+++ src/components/OrderReview/OrderReview.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faDeleteLeft, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-const OrderReview = ({pd}) => {
+const OrderReview = ({pd,removeFromCartBtn}) => {
     console.log(pd)
-    const{img,id, price,quantity}=pd
+    const{img,id, price,quantity,name}=pd
     return (
-        <div>
-            <div className='border-2 flex items-center '>
+        <div className='flex border-2 m-5'  >
+            <div className=' items-center p-5 '>
                 <img className='w-32 h-32 ' src={img} alt="" />
 
-               <div className='flex items-center justify-between'>
+            </div>
+            
+            <div className='grid grid-cols-2 gap-96  items-center justify-between  '>
                <div className='ml-5'>
-               <h3>Price: {price}</h3>
-                <h3>Quantity: {quantity}</h3>
+                <h1 className='text-lg font-bold'>{name}</h1>
+               <h3 className='font-semibold'>Price: ${price}</h3>
+                <h3 className='font-semibold'>Quantity: {quantity}</h3>
                </div>
                <div>
-                
-               <button><FontAwesomeIcon className='text-2xl' icon={faCoffee} /></button>
+               <button onClick={()=>removeFromCartBtn(id)} className='border-2 border-red-200 p-3 rounded-full bg-red-300 text-center hover:border-red-600'><FontAwesomeIcon className='text-4xl text-red-800 text-center' icon={faTrash} /></button>
                </div>
                </div>
-            </div>
         </div>
     );
 };
--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import { useLoaderData } from 'react-router-dom';
 import OrderReview from '../OrderReview/OrderReview';
+import { removeFromDb } from '../Utilities/fakedb';
 
 const Orders = () => {
     const products = useLoaderData()
+    const [cart,setCart]=useState(products)
+
+    const removeFromCartBtn =(id)=>{
+        const remaining = cart.filter(pd => pd.id !== id)
+        setCart(remaining)
+        removeFromDb(id)
+
+    }
 
     return (
         <div className='flex w-full max-w-7xl mx-auto mt-12  '>
             <div className='w-[80%] max-w-4xl mx-auto p-5'>
                 {
-                    products.map(pd => <OrderReview key={pd.id} pd={pd}></OrderReview>)
+                   cart.map(pd => <OrderReview key={pd.id} pd={pd} removeFromCartBtn={removeFromCartBtn}></OrderReview>)
                 }
 
             </div>
 
             <div className='w-[20%] bg-amber-200 rounded'>
-            <OrderSummary item={products}></OrderSummary>
+            <OrderSummary item={cart}></OrderSummary>
            
 
             </div>
